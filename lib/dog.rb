f3df36eb7d93f4728373d1a8d47af29c1c623a35
omar3o3 <otantawy53@gmail.com>
2022-06-08 13:13:49 -0400

    commit tree 22c479b0de21582d09a40b6f3aa7df9593cf0638

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
-class Dog
+require 'pry'
 
+class Dog
+    # attr_reader :name
+    # attr_writer :name
+    attr_accessor :name
+
+    def initialize(value)
+        @name = value
+    end
 end
+
+# lassie = Dog.new
+# lassie.name = "Lassie"
+# lassie.display_name
+
+lassie = Dog.new('Lassie')
+
+binding.pry
+0
